1fd5ed3699f5084e29375dd3225fac185b9b0bbe
empljx <empljx@sina.com>
2014-12-12 16:39:00 +0800

    yes

---
+++ src/main/java/com/empljx/util/Test.java
@@ -0,0 +1,14 @@
+package com.empljx.util;
+
+/**
+ * Date: 14/12/11 обнГ5:21
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        new Test();
+
+        System.out.println("fds");
+    }
+
+}
